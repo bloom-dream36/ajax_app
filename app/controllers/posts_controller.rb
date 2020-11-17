3811895e4acf5adb558a025831e09f5b50bba975
bloom-dream36 <dream.2001.samu@icloud.com>
2020-11-17 18:06:32 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,10 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.create(content:params[:content])
-    redirect_to action: :index
+    # メモ作成時に未読の情報を保存するようにしたこと
+    post = Post.create(content:params[:content], checked:false)
+    # レスポンスをJSONに変更した
+    render json:{ post: post }
   end
 
   def checked
